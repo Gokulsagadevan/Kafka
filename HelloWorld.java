cf5114d533a63896cd8ee1244dacc77416629371
Gokul Sagadevan <53510140+Gokulsagadevan@users.noreply.github.com>
2024-11-13 14:32:06 +0000

    inital commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// This is a basic Java program
+public class HelloWorld {
+
+    // Main method - execution entry point of the program
+    public static void main(String[] args) {
+        
+        // Print a message to the console
+        System.out.println("Hello, World!");
+    }
+}
